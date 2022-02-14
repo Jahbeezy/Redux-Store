6b45a6302cb05d4cbff73b6e4d826d6d8fe8cd6c
Justin Barner <justinrb14@gmail.com>
2022-02-14 02:52:31 -0800

    initial commit

---
+++ client/build/precache-manifest.78fb03f377fd8f915b5250d7bfb33494.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a29f27d85a0ea1c862e6137119ca52dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3cb9d79a8b63d00635bc",
+    "url": "/static/css/main.a64bd9e7.chunk.css"
+  },
+  {
+    "revision": "908a4a63b8dfa18bc627",
+    "url": "/static/js/2.a7dad216.chunk.js"
+  },
+  {
+    "revision": "aa4100970f46d7e0ec2af84de3f2740b",
+    "url": "/static/js/2.a7dad216.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3cb9d79a8b63d00635bc",
+    "url": "/static/js/main.909036e1.chunk.js"
+  },
+  {
+    "revision": "02751e160f988a50d44f",
+    "url": "/static/js/runtime-main.8a258a78.js"
+  },
+  {
+    "revision": "ab4978553bd68ca15e503a0da7fa604d",
+    "url": "/static/media/spinner.ab497855.gif"
+  }
+]);
